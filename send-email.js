31e9a70f706e1cbc78995ef7625630d19b2437cf
DaleJoseph <dalejoseph@DaleJosephs-MacBook-Air.local>
2024-09-10 23:09:54 +0800

    done fixing the navbar, finish testing the send email, done setting up the contact form, and figure it out how to make the website responsive

---
+++ send-email.js
@@ -0,0 +1,28 @@
+const nodemailer = require("nodemailer");
+
+const transporter = nodemailer.createTransport({
+  host: "smtp.resend.com",
+  port: 465,
+  secure: true, // true for port 465, false for other ports
+  auth: {
+    user: "resend",
+    pass: "re_2MXp5V7S_kes55REoWUFysR13gq3Nb2hU",
+  },
+});
+
+// async..await is not allowed in global scope, must use a wrapper
+async function Email() {
+  // send mail with defined transport object
+  const info = await transporter.sendMail({
+    from: '"OnBoarding" <delivered@resend.dev>', // sender address
+    to: "silicon.logic.tech@gmail.com", // list of receivers
+    subject: "Hello ✔", // Subject line
+    text: "Hello world?", // plain text body
+    html: "<b>Hello world?</b>", // html body
+  });
+
+  console.log("Message sent: %s", info.messageId);
+  // Message sent: <d786aa62-4e0a-070a-47ed-0b0666549519@ethereal.email>
+}
+
+Email().catch(console.error);
