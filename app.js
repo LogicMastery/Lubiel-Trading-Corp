eb3103bfbfbff51b1e8f832d9b51bbab9b97f695
LogicMastery <silicon.logic.tech@gmail.com>
2024-09-13 19:34:56 +0800

    test

---
+++ app.js
@@ -0,0 +1,8 @@
+const ActivePage = window.location.pathname;
+const navLinks = document.querySelectorAll('nav a').forEach(link => {
+    if(link.href.includes(`${ActivePage}`)){
+        link.classList.add('active');
+    }
+});
+
+
