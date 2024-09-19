3ef6a08ff6fc8019df832880cc0c580dd3747457
LogicMastery <silicon.logic.tech@gmail.com>
2024-09-19 18:05:38 +0800

    added the resend code in the server.js file

--- server.js
+++ server.js
@@ -1,25 +1,29 @@
 // Resend information
-const nodemailer = require('nodemailer');
+const nodemailer = require("nodemailer");
 
 const transporter = nodemailer.createTransport({
-    host: "smtp.resend.com",
-    port: "465",
-    auth: {
-        user: "resend",
-        pass: "re_D4A82adt_2BLaLBqHQ5hz9UT6jwDgYZRE"
-    },
+  host: "smtp.resend.com",
+  port: 465,
+  secure: true, // true for port 465, false for other ports
+  auth: {
+    user: "resend",
+    pass: "re_D4A82adt_2BLaLBqHQ5hz9UT6jwDgYZRE",
+  },
 });
 
-    async function submit() {
-        const info = await transporter.sendMail({
-            from: '"Resend " <delivered@resend.dev>',
-            to: "silicon.logic.tech@gmail.com",
-            subject: "Test",
-            text: "Did it work?"
-        });
-        console.log("Message sent: %s", info.messageId);
-  // Message sent: <d786aa62-4e0a-070a-47ed-0b0666549519@resend.email>
+// async..await is not allowed in global scope, must use a wrapper
+async function main() {
+  // send mail with defined transport object
+  const info = await transporter.sendMail({
+    from: 'delivered@resend.dev', 
+    to: "silicon.logic.tech@gmail.com", 
+    subject: "test", 
+    text: "Did it work?", 
+    html: "<b>Test</b>",
+  });
 
-    }
+  console.log("Message sent: %s", info.messageId);
+  // Message sent: <d786aa62-4e0a-070a-47ed-0b0666549519@ethereal.email>
+}
 
-    submit().catch(console.error);
+main().catch(console.error);
