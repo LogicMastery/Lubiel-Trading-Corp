c50b038bd20e807e231a6a484e8e38c2d7713468
LogicMastery <silicon.logic.tech@gmail.com>
2024-09-19 14:15:25 +0800

    added a comment in my app.js file

--- app.js
+++ app.js
@@ -1,4 +1,4 @@
-// selected links that you are currently on
+// shows the selected links you are currently selected in the nav links
 const ActivePage = window.location.pathname;
 const navLinks = document.querySelectorAll('nav a').forEach(link => {
     if(link.href.includes(`${ActivePage}`)){
